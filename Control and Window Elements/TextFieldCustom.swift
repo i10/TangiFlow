7d087806d1645e30efa8142119a752cb1bd5e533
asifmayilli <mayilli92@gmail.com>
2019-12-16 11:58:47 +0100

    Update TextFieldCustom.swift

--- Control and Window Elements/TextFieldCustom.swift
+++ Control and Window Elements/TextFieldCustom.swift
@@ -6,21 +6,22 @@
 //  Copyright © 2019 RWTH Aachen University. All rights reserved.
 //
 
+
+//it is custom textfield class which has id, type and parent fields added
 import Foundation
 import AppKit
 class CustomTextFields:NSTextView{
     var id:String?
+    //type of input "text" for alpha numerical and "number" for numerical keyboard
     var type:String = "text"
+    //stores node to which text field belongs
     var parent:Node? = nil
     override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
         super.init(frame:frameRect,textContainer: container)
         
     }
     
-//    convenience init(frame frameRect: NSRect, id:String) {
-//        self.init(frame:frameRect)
-//        self.id = id
-//    }
+
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
