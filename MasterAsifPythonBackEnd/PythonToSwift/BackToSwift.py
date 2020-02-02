29fd634151fdca08bb3db3c0d30d0fd4e5c15565
asifmayilli <mayilli92@gmail.com>
2020-02-02 20:14:35 +0100

    Update BackToSwift.py
    comments for BackToSwift

--- MasterAsifPythonBackEnd/PythonToSwift/BackToSwift.py
+++ MasterAsifPythonBackEnd/PythonToSwift/BackToSwift.py
@@ -8,6 +8,23 @@ class PlotTypeNotProvided(Exception):
 
 class BackToSwift:
     result_maker = None
+    """ 
+    This function handles file creation for all data types
+  
+    Parameters: 
+    node_id (str): id of the node
+    
+    type (ResultType): which type of data node represents
+    
+    data: result data which should be displayed. In case no data available leave it None
+    
+    error (str): in case of exception error message of exception should be provided
+    
+    tool_type (str): different libraries can have different ways of saving data. 
+                     In our base app  we use cv2 and Pillow for image editing and they save files differently. 
+                     To handle this we provide which tool we are using. 
+    extension (str): extension of the result image, audio or other media file
+    """
     def prepare_data(self,type=ResultType.generic_python.value,node_id="",\
                      data=None,error=None,\
                      plot_type=None,project_name="",\
