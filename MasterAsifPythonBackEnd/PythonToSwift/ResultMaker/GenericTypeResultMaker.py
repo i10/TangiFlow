db185180109e1ef1ccbae5697d0e92ff4d0f12de
asifmayilli <mayilli92@gmail.com>
2020-02-02 20:06:21 +0100

    Update GenericTypeResultMaker.py
    comment for GenericTypeResultMaker

--- MasterAsifPythonBackEnd/PythonToSwift/ResultMaker/GenericTypeResultMaker.py
+++ MasterAsifPythonBackEnd/PythonToSwift/ResultMaker/GenericTypeResultMaker.py
@@ -2,10 +2,19 @@ import json
 from ROOT import ROOT_DIR
 from PythonToSwift.ResultMaker.ResultMakerBase import ResultMakerBase
 
-
 class GenericResultMaker(ResultMakerBase):
+    """ 
+    This function is responsible for creating resulting JSON file for a node with given id 
+    for generic python data type such as integer, string and etc.
+  
+    Parameters: 
+    id (str): id of the node
+    data (generic python type): result data which should be displayed. In case no data available leave it None
+    error (str): in case of exception error message of exception should be provided
+    project_name (str): name of the project. can be left empty. it is optional
+    """
     def create_file(self,id="",data=None,error=None,project_name=""):
-
+    
         if not error:
             json_data = {"project_name":project_name,\
                          "error":False ,\
